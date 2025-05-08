d9adbb874f5e1eed600aee07b4a6439396078b6a
wyo <seikimatu4649@gmail.com>
2025-05-09 00:20:54 +0900

    add main

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,57 @@
 mod token;
+mod scanner;
+use std::io::{self, Write};
+
+// todo replを実装する
 fn main() {
-    println!("Hello, world!");
+  let args: Vec<String> = std::env::args().collect();
+  if args.len() == 1 {
+    let path = &args[1];
+    run_file(path);
+  } else {
+    run_prompt();
+  }
+}
+
+
+fn run_file(path: &str) {
+  let source = std::fs::read_to_string(path).expect("Failed to read file");
+  run(&source);
+}
+
+fn run_prompt() {
+  loop {
+    print!("> ");
+    io::stdout().flush().unwrap();
+    let mut line = String::new();
+    let bytes = io::stdin().read_line(&mut line).expect("Failed to read line");
+    if bytes == 0 {
+      println!("exiting...");
+      break;
+    }
+    let line = line.trim();
+    println!("You entered: {}", line);
+    run(line);
+  }
+}
+
+fn run(source: &str) {
+  let mut scanner = scanner::Scanner::new(source);
+  // todo err handling
+  let tokens = scanner.scan_tokens().unwrap();
+
+  for token in tokens {
+    println!("{:?}", token);
+  }
+
+  // match reader.read() {
+  //   Ok(_) => println!("Read successfully"),
+  //   Err(e) => println!("Error reading: {}", e),
+  // }
+}
+
+// todo: エラートレイトを実装する
+fn err(line: usize,column: usize , where_: String, msg: &str) {
+  eprintln!("[line \"{} \": column \"{}\"] {} : {}", line, column, where_ , msg);
+  // todo 視覚的にわかりやすいように場所も表示する
 }
