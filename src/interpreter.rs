565cd8d0bdc7f1d28d2e0b136c870475e5c47cb3
wyo <seikimatu4649@gmail.com>
2025-05-14 22:23:29 +0900

    add interprit

---
+++ src/interpreter.rs
@@ -0,0 +1,205 @@
+use crate::expr::{Literal, Grouping, Binary, Unary, Expr, UnaryOperator, BinaryOperator};
+use crate::token::Token;
+use std::{f32::consts::E, fmt};
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum RunTimeError {
+  UnaryError {
+    token: Token,
+    message: String,
+  },
+  BinaryError {
+    left: Token,
+    right: Token,
+    operator: String,
+    message: String,
+  },
+}
+
+impl fmt::Display for RunTimeError {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    match self {
+      RunTimeError::UnaryError {token,  message } => {
+        write!(f, "Run-time error at {}:{}: {}: {}", token.line, token.column, token.lexeme, message)
+      }
+      RunTimeError::BinaryError { left, right, operator ,message } => {
+        write!(f, "Run-time error at {}:{}: {} {} {}: {}", left.line, left.column, left.lexeme, operator, right.lexeme, message)
+      }
+    }
+
+  }
+}
+
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Value {
+  Number(f64),
+  String(String),
+  Boolean(bool),
+  Nil,
+}
+
+#[derive(Debug)]
+pub struct interpreter {}
+
+type InterpretResult = Result<Value, RunTimeError>;
+impl interpreter {
+  pub fn new() -> Self {
+    interpreter {}
+  }
+
+  pub fn interpret(&self, expr: &Expr) {
+    let res = self.interpret_expr(expr);
+    if let Err(err) = res {
+    eprintln!("{}", err);
+    } else {
+      println!("{:?}", res);
+    }
+  }
+
+  pub fn interpret_expr(&self, expr: &Expr) -> InterpretResult {
+    match expr {
+      Expr::Literal(literal) => self.interpret_literal(literal),
+      Expr::Grouping(grouping) => self.interpret_expr(&grouping.expression),
+      Expr::Unary(unary) => self.interpret_unary(unary),
+      Expr::Binary(binary) => self.interpret_binary(binary),
+    }
+  }
+
+  fn interpret_literal(&self, literal: &Literal) -> InterpretResult {
+    match literal {
+      Literal::String(s) => Ok(Value::String(s.clone())),
+      Literal::Number(n) => Ok(Value::Number(*n)),
+      Literal::False => Ok(Value::Boolean(false)),
+      Literal::True => Ok(Value::Boolean(true)),
+      Literal::Nil => Ok(Value::Nil),
+    }
+  }
+
+  fn interpret_unary(&self, unary: &Unary) -> InterpretResult {
+    let right = self.interpret_expr(&unary.right)?;
+    match unary.operator {
+      UnaryOperator::Minus => match right {
+        Value::Number(n) => Ok(Value::Number(-n)),
+        _ => Err(RunTimeError::UnaryError {
+          token: unary.token.clone(),
+          message: "operator '-' requires a number".to_string(),
+        }),
+      },
+      UnaryOperator::Bang => Ok(Value::Boolean(!self.is_truthy(&right))),
+    }
+  }
+
+  fn interpret_binary(&self, binary: &Binary) -> InterpretResult {
+    let left = self.interpret_expr(&binary.left)?;
+    let right = self.interpret_expr(&binary.right)?;
+
+    match binary.operator {
+      BinaryOperator::Greater => match (left, right) {
+        (Value::Number(l), Value::Number(r)) => Ok(Value::Boolean(l > r)),
+        _ => Err(RunTimeError::BinaryError {
+          message: "operator '>' requires two numbers".to_string(),
+          left: binary.left_token.clone(),
+          right: binary.right_token.clone(),
+          operator: ">".to_string(),
+        }),
+      },
+      BinaryOperator::GreaterEqual => match (left, right) {
+        (Value::Number(l), Value::Number(r)) => Ok(Value::Boolean(l >= r)),
+        _ => Err(RunTimeError::BinaryError {
+          message: "operator '>=' requires two numbers".to_string(),
+          left: binary.left_token.clone(),
+          right: binary.right_token.clone(),
+          operator: ">=".to_string(),
+        }),
+      },
+      BinaryOperator::Less => match (left, right) {
+        (Value::Number(l), Value::Number(r)) => Ok(Value::Boolean(l < r)),
+        _ => Err(RunTimeError::BinaryError {
+          message: "operator '<' requires two numbers".to_string(),
+          left: binary.left_token.clone(),
+          right: binary.right_token.clone(),
+          operator: "<".to_string(),
+        }),
+      },
+      BinaryOperator::LessEqual => match (left, right) {
+        (Value::Number(l), Value::Number(r)) => Ok(Value::Boolean(l <= r)),
+        _ => Err(RunTimeError::BinaryError {
+          message: "operator '<=' requires two numbers".to_string(),
+          left: binary.left_token.clone(),
+          right: binary.right_token.clone(),
+          operator: "<=".to_string(),
+        }),
+      },
+      BinaryOperator::Minus => match (left, right) {
+        (Value::Number(l), Value::Number(r)) => Ok(Value::Number(l - r)),
+        _ => Err(RunTimeError::BinaryError {
+          message: "operand '-' requires two numbers".to_string(),
+          left: binary.left_token.clone(),
+          right: binary.right_token.clone(),
+          operator: "-".to_string(),
+        }),
+      },
+      BinaryOperator::Plus => match (left, right) {
+        (Value::Number(l), Value::Number(r)) => Ok(Value::Number(l + r)),
+        (Value::String(l), Value::String(r)) => Ok(Value::String(format!("{}{}", l, r))),
+        _=> Err(RunTimeError::BinaryError {
+          message: "operand '+' requires two numbers or two strings".to_string(),
+          left: binary.left_token.clone(),
+          right: binary.right_token.clone(),
+          operator: "+".to_string(),
+        }),
+      }
+      BinaryOperator::Star => match (left, right) {
+        (Value::Number(l), Value::Number(r)) => Ok(Value::Number(l * r)),
+        _ => Err(RunTimeError::BinaryError {
+          message: "operand '*' requires two numbers".to_string(),
+          left: binary.left_token.clone(),
+          right: binary.right_token.clone(),
+          operator: "*".to_string(),
+        }),
+      }
+      BinaryOperator::Slash => match (left, right) {
+        (Value::Number(l), Value::Number(r)) => {
+          if r == 0.0 {
+            Err(RunTimeError::BinaryError {
+              message: "Division by zero".to_string(),
+              left: binary.left_token.clone(),
+              right: binary.right_token.clone(),
+              operator: "/".to_string(),
+            })
+          } else {
+            Ok(Value::Number(l / r))
+          }
+        }
+        _ => Err(RunTimeError::BinaryError {
+          message: "operand '/' requires two numbers".to_string(),
+          left: binary.left_token.clone(),
+          right: binary.right_token.clone(),
+          operator: "/".to_string(),
+        }),
+      }
+      BinaryOperator::EqualEqual => Ok(Value::Boolean(self.is_equal(&left, &right))),
+      BinaryOperator::BangEqual =>  Ok(Value::Boolean(!self.is_equal(&left, &right))),
+    }
+  }
+
+  fn is_truthy(&self, value: &Value) -> bool {
+    match value {
+      Value::Nil => false,
+      Value::Boolean(b) => *b,
+      _ => true,
+    }
+  }
+
+  fn is_equal(&self, a: &Value, b: &Value) -> bool {
+    match (a,b) {
+      (Value::Number(a), Value::Number(b)) => a == b,
+      (Value::String(a), Value::String(b)) => a == b,
+      (Value::Nil, Value::Nil) => true,
+      (Value::Boolean(a), Value::Boolean(b)) => a == b,
+      _ => false,
+    }
+  }
+
+}
